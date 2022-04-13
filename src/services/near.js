fe72a5d7e1873c350099908dcc21d8cdfb06109a
OlexandrSai <38455192+OlexandrSai@users.noreply.github.com>
2022-04-13 15:50:04 +0100

    services layer finished

--- src/services/near.js
+++ src/services/near.js
@@ -1,25 +1,55 @@
 import { keyStores, Near, WalletConnection, utils } from "near-api-js";
 import BN from "bn.js";
 
-export const CONTRACT_ID = "dev-1634131199153-23397978180139";
-export const REGISTRY_CONTRACT_ID = "dev-1631276003404-61262416620213";
-const gas = new BN("70000000000000");
+export const THANKS_CONTRACT_ID = process.env.VUE_APP_THANKS_CONTRACT_ID;
+export const REGISTRY_CONTRACT_ID = process.env.VUE_APP_REGISTRY_CONTRACT_ID;
+const gas = new BN(process.env.VUE_APP_gas);
 
 
 // new NEAR is using  here to  awoid  async/await
 export const near = new Near({
-    networkId: "testnet",
+    networkId: process.env.VUE_APP_networkId,
     keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-    nodeUrl: "https://rpc.testnet.near.org",
-    walletUrl: "https://wallet.testnet.near.org",
-  });
+    nodeUrl: process.env.VUE_APP_nodeUrl,
+    walletUrl: process.env.VUE_APP_walletUrl,
+});
 
 export const wallet = new WalletConnection(near, "thankyou");
 
-//function to get all recipients from registry contract
+// --------------------------------------------------------------------------
+// functions to call contracts(Registry, Thanks) Public VIEW methods
+// --------------------------------------------------------------------------
+
+// functions to call REGISTRY contract public view methods
+// --------------------------------------------------------------------------
+
+// function to get all thanks contracts ids which were added to the registry contract
 export const getRecipients = () => {
     return wallet.account().viewFunction(REGISTRY_CONTRACT_ID, "list_all");
-};
+};  
+
+// function to check is the contract id registered inside REGISTRY contract state
+export const isRegistered = (contractId) => {
+    return wallet.account().viewFunction(REGISTRY_CONTRACT_ID, "is_registered", {contract: contractId});
+}
+
+// --------------------------------------------------------------------------
+// functions to call contracts(Registry, Thanks) Public CHANGE methods
+// --------------------------------------------------------------------------
+
+// functions to call THANKS contract public change methods
+// --------------------------------------------------------------------------
+
+//
+export const registerNewThanksContract = ({ message, anonymous, attachedDeposit }) => {
+    attachedDeposit = utils.format.parseNearAmount(attachedDeposit)
+    return wallet.account().functionCall({
+        contractId: CONTRACT_ID,
+        methodName: "say",
+        args: { message, anonymous },
+        attachedDeposit: attachedDeposit
+    })
+}
 
 //function to get all messages from thankyou contract
 export const getMessages = () => {
@@ -33,12 +63,12 @@ export const transfer = () => {
 }
 
 //function to sendMessage
-export const sendMessage = ({message,anonymous,attachedDeposit}) => {
+export const sendMessage = ({ message, anonymous, attachedDeposit }) => {
     attachedDeposit = utils.format.parseNearAmount(attachedDeposit)
     return wallet.account().functionCall({
         contractId: CONTRACT_ID,
         methodName: "say",
-        args: {message, anonymous },
-        attachedDeposit:attachedDeposit
+        args: { message, anonymous },
+        attachedDeposit: attachedDeposit
     })
 }
